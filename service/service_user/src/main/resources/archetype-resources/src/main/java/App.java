985368d5433ec7da7a5e307adab09402b7af8fd3
Nar1s0ahz <1303628536@qq.com>
2024-02-28 16:38:19 +0800

    第一次提交

---
+++ service/service_user/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.zsr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
